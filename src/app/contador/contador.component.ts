7935dc2dff2e25f874ab88a0ddf6e6c74b236c77
Urian <urian1213viera@gmail.com>
2021-04-03 11:11:31 -0500

    funcionando acumulador

--- src/app/contador/contador.component.ts
+++ src/app/contador/contador.component.ts
@@ -9,6 +9,22 @@ export class ContadorComponent implements OnInit {
 
   constructor() { }
 
+  numero:number = 15;
+
+  sumar(){
+    this.numero +=1;
+  }
+
+  resta(){
+    this.numero -=1;
+  }
+
+
+  //espera un numero como respuesta
+  accionOperacion( valor:number ){
+    this.numero +=valor;
+  }
+
   ngOnInit(): void {
   }
 
