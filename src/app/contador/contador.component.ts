7be133a29bc96da6911d18d6d0bce7562e2da9f1
Urian <urian1213viera@gmail.com>
2021-04-03 10:17:50 -0500

    Iniciando el proyecto

---
+++ src/app/contador/contador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  templateUrl: './contador.component.html',
+  styleUrls: ['./contador.component.css']
+})
+export class ContadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
